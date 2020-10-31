e45f33a907efb33158aa31888ab2270b2d8b12a9
Matias Bzurovski <matibzurovski@gmail.com>
2020-10-31 15:12:51 -0300

    Using cache for images

--- RedditClient/Core/Extensions/UIKit+RedditClient.swift
+++ RedditClient/Core/Extensions/UIKit+RedditClient.swift
@@ -7,14 +7,25 @@
 
 import UIKit
 
+fileprivate let imageCache = NSCache<AnyObject, AnyObject>()
+
 extension UIImageView {
     
     func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
+        if let imageFromCache = imageCache.object(forKey: url as AnyObject) as? UIImage {
+            /// Image is available on cache
+            DispatchQueue.main.async {
+                self.image = imageFromCache
+            }
+        } else {
+            /// Image is not available on cache, so we will download it and save it for future use.
+            DispatchQueue.global().async { [weak self] in
+                if let data = try? Data(contentsOf: url) {
+                    if let image = UIImage(data: data) {
+                        DispatchQueue.main.async {
+                            self?.image = image
+                        }
+                        imageCache.setObject(image, forKey: url as AnyObject)
                     }
                 }
             }
